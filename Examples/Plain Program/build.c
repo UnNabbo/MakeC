00ac6287ece6606aafca66e78898e7f16a1f04a5
Nabbo <saveriocioccolini@gmail.com>
2024-09-27 18:44:29 +0200

    rewrite

--- Examples/Plain Program/build.c
+++ Examples/Plain Program/build.c
@@ -1,10 +1,14 @@
+#define ENTRY_POINT Entry
+#define ENABLE_QUIET_MODE 
 #include "../../makec.h"
 
-int main(void){
-	project Project = NewProject("HelloMakeC", OUTPUT_KIND_EXECUTABLE, COMPILER_KIND_MSVC);
-	AddFileToProject(&Project, "main.c");
+void Entry(){
+	project Engine = ProjectCreate("Engine", COMPILER_MSVC, OUTPUT_EXECUTABLE);
+	ProjectSetFilePath(&Engine, "src"); // EQUIVALENT OF DOING: Engine.FilePath = STR("src") 
+	ProjectSetOutputPath(&Engine, "out");// EQUIVALENT OF DOING: Engine.OutputPath = STR("out") 
 	
-	int ErrorCode = 0;
-	CompileProjectAndWait(&Project, &ErrorCode,  COMPILATION_SETTINGS_NONE);
-	return 0;							
+	ProjectAddSourceFiles(&Engine, "main.c");
+	
+	ProjectAddCompilerFlags(&Engine, "/Zi /nologo");
+	ProjectCompileAndWait(&Engine);
 }
--- Examples/Plain Program/makebuild.bat
+++ Examples/Plain Program/makebuild.bat
@@ -1,4 +1,6 @@
 @echo off
-cl /nologo /std:c++20 /W0 /EHsc /O2 /external:W0 /cgthreads8 build.c /link /OUT:build.exe
+cl /nologo /std:c++20 /O2 /W0 /EHsc /external:W0 /cgthreads8 build.c /link /OUT:build.exe
+
+
 
 ::gcc build.c -O3 -lstdc++ -o build.exe
